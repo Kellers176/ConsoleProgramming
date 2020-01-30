00313ee52f761bb851c0c376aa3f90430c17bf1c
kelly <kiherstine@gmail.com>
2020-01-30 11:03:49 -0500

    Forgot to commint update

---
+++ WebsiteFiles/js/main.js
@@ -0,0 +1,17 @@
+/*$(function() {
+	var c = document.getElementById("myCanvas");
+	var ctx = c.getContext("2d");
+	ctx.beginPath();
+	ctx.arc(95, 50, 40, 0, 2 * Math.PI);
+	ctx.stroke();
+});
+  */
+function startGame() {
+	//alert("Reached Javascript");
+	var c = document.getElementById("myCanvas");
+	var ctx = c.getContext("2d");
+	ctx.beginPath();
+	ctx.arc(95, 50, 40, 0, 2 * Math.PI);
+	ctx.stroke();
+}
+
