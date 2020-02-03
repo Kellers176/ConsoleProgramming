670338adeeb767b084ab7353cf83cff3ec13a186
Kelly <kiherstine@gmail.com>
2020-02-03 15:13:42 -0500

    Got working and moving sprites in, will need to change the way the player is being drawn

--- WebsiteFiles/js/main.js
+++ WebsiteFiles/js/main.js
@@ -1,21 +1,33 @@
+//Global variables
+//position fram will be drawn
 var x = 0;
 var y = 0;
-	
+//where the frame currently is
 var srcX;
 var srcY;
-	
+//Get the sheet width of the sprite sheet
 var sheetWidth = 864;
-var sheetHeight = 582;
-	
+var sheetHeight = 300;
+//the amount of rows and columns of the sprite sheet
 var cols = 8;
-var rows = 8;
-	
+var rows = 2;
+//how many frames in the sprite sheet
+var frameCount = 8;
+var currentFrame = 0;
+//position the character is facing by changing the row
+var trackLeft = 1;
+var trackRight = 0;
+
+//the width and the height of each sprite
 var width = sheetWidth / cols;
 var height = sheetHeight / rows;
 
+//reference the game canvas
 var game_canvas = document.getElementById("myCanvas");
 var ctx = game_canvas.getContext("2d");
-
+//the player character
+var character = new Image();
+character.src = "images/character.png";
 
 function SetBackground()
 {
@@ -31,25 +43,24 @@ function SetBackground()
 
 function updateFrame()
 {
-
-	var currentFrame = 0;
+    //calculate the current frame
 	currentFrame = ++currentFrame % cols; 
 	
+    //update the current frame
 	srcX = currentFrame * width;
-	srcY = 0;
+	srcY = trackRight * height;
+    
+    //clear the renderer
+    ctx.clearRect(x,y,width, height);
 	
 }
 
 function drawImage()
 {
-	var character = new Image();
-	character.src = "character.png";
-
+    //updater and render image
 	updateFrame();
 	ctx.drawImage(character, srcX, srcY, width, height, x, y, width, height);
+    
 }
 
-setInterval(function()
-{
-	drawImage();
-}, 100);
+
