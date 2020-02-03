8371decf6b7599edaea38d0dca0cad87e4f936d9
kelly <kiherstine@gmail.com>
2020-02-03 13:12:49 -0500

    Got a background drawing on my canvas

--- WebsiteFiles/js/main.js
+++ WebsiteFiles/js/main.js
@@ -1,17 +1,55 @@
-/*$(function() {
-	var c = document.getElementById("myCanvas");
-	var ctx = c.getContext("2d");
-	ctx.beginPath();
-	ctx.arc(95, 50, 40, 0, 2 * Math.PI);
-	ctx.stroke();
-});
-  */
-function startGame() {
-	//alert("Reached Javascript");
-	var c = document.getElementById("myCanvas");
-	var ctx = c.getContext("2d");
-	ctx.beginPath();
-	ctx.arc(95, 50, 40, 0, 2 * Math.PI);
-	ctx.stroke();
+var x = 0;
+var y = 0;
+	
+var srcX;
+var srcY;
+	
+var sheetWidth = 864;
+var sheetHeight = 582;
+	
+var cols = 8;
+var rows = 8;
+	
+var width = sheetWidth / cols;
+var height = sheetHeight / rows;
+
+var game_canvas = document.getElementById("myCanvas");
+var ctx = game_canvas.getContext("2d");
+
+
+function SetBackground()
+{
+	//document.getElementById("myCanvas").style.background = "url('images/environment.jpg')";
+	var background = new Image();
+	background.src = "images/environment.jpg";
+	background.onload = function() {
+		game_canvas.width = background.width;
+  		game_canvas.height = background.height;
+		ctx.imageSmoothingEnabled = true;
+    	ctx.drawImage(background, 0, 0, game_canvas.width, game_canvas.height);}
 }
 
+function updateFrame()
+{
+
+	var currentFrame = 0;
+	currentFrame = ++currentFrame % cols; 
+	
+	srcX = currentFrame * width;
+	srcY = 0;
+	
+}
+
+function drawImage()
+{
+	var character = new Image();
+	character.src = "character.png";
+
+	updateFrame();
+	ctx.drawImage(character, srcX, srcY, width, height, x, y, width, height);
+}
+
+setInterval(function()
+{
+	drawImage();
+}, 100);
