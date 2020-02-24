2ff9ec8d9ffee896a90136d02a5b7129c31e87a7
Kelly <kiherstine@gmail.com>
2020-02-24 15:16:17 -0500

    Finally got two canvass working!

--- WebsiteFiles/js/Bow.js
+++ WebsiteFiles/js/Bow.js
@@ -1,17 +1,18 @@
 //I think i need to use css
-
-
-var canvas2 = document.getElementById("myBowCanvas");
-var ctx2 = canvas2.getContext("2d");
-
+layer2 = document.getElementById("myBowCanvas");
+            ctx2 = layer2.getContext("2d");
 function RotateCanvas()
 {
-    //ctx.rotate(20 * Math.PI / 180);
+    //ctx2.rotate(20 * Math.PI / 180);
     var bow = new Image();
     bow.src = "images/bowAndHands.png";
+   
     bow.onload = function () 
     {
-        ctx2.drawImage(bow, 0, 0, 120, 120, 0, 0, 200, 120);
-       
+        bow.width = characterWidth;
+        bow.height = characterHeight * 0.7;
+        ctx2.imageSmoothingEnabled = true;
+        ctx2.drawImage(bow, 0, 0, 120, 120, 10, 670, bow.width, bow.height);
+        //ctx2.rotate(40);
     }
 }
