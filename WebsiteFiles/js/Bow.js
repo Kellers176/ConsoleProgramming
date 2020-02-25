ace610be04b927fcf9e568aef46b5be6d5ac2b8a
Kelly <kiherstine@gmail.com>
2020-02-24 19:32:31 -0500

    Got rotation working!

--- WebsiteFiles/js/Bow.js
+++ WebsiteFiles/js/Bow.js
@@ -1,12 +1,24 @@
 //I think i need to use css
 layer2 = document.getElementById("myBowCanvas");
-            ctx2 = layer2.getContext("2d");
+ctx2 = layer2.getContext("2d");
+
+var x = 10;
+var y = 660;
+
+var cx = x + 0.5 * 120;
+var cy = y + 0.5 * 120;
+
+var angle = -25;
+
 function RotateCanvas()
 {
-    //ctx2.rotate(20 * Math.PI / 180);
     var bow = new Image();
     bow.src = "images/bowAndHands.png";
-   
+    //rotate
+    ctx2.translate(cx,cy);
+    ctx2.rotate( (Math.PI / 180) * angle);
+    ctx2.translate(-cx,-cy);
+    //draw
     bow.onload = function () 
     {
         bow.width = characterWidth;
@@ -15,4 +27,8 @@ function RotateCanvas()
         ctx2.drawImage(bow, 0, 0, 120, 120, 10, 670, bow.width, bow.height);
         //ctx2.rotate(40);
     }
+    
+    //ctx2.rotate(20/20);
+   // ctx2.rotate(4* Math.PI / 180);
+
 }
