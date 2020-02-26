67a4734a77094049d4d891c083f4e386cd05c9b8
Kelly <kiherstine@gmail.com>
2020-02-26 11:54:26 -0500

    Trying to get the angle of the angle of the mouse and the bow, somewhat working but not really

--- WebsiteFiles/js/Bow.js
+++ WebsiteFiles/js/Bow.js
@@ -8,13 +8,21 @@ var y = 660;
 var cx = x + 0.5 * 120;
 var cy = y + 0.5 * 120;
 
-var angle = -25;
+var angle = -25 ;
 
 function RotateCanvas()
 {
     var bow = new Image();
     bow.src = "images/bowAndHands.png";
     //rotate then move back to original position
+    //this might need to be on an interval
+    var mytimer = setInterval(function dothis(){
+        //document.getElementById('safeTimerDisplay').innerHTML='00:'+sec;
+		ctx2.translate(cx,cy);
+        ctx2.rotate( (Math.PI / 180) * angle);
+        ctx2.translate(-cx,-cy);
+        
+    }, 1000);
     ctx2.translate(cx,cy);
     ctx2.rotate( (Math.PI / 180) * angle);
     ctx2.translate(-cx,-cy);
--- WebsiteFiles/js/Mouse.js
+++ WebsiteFiles/js/Mouse.js
@@ -1,15 +1,35 @@
 var mousePosX = 0;
 var mousePosY = 0;
-
+var thisAngle = 0;
+var finalAngle = 0;
 
 function GetMouseCoords()
 {
 	window.addEventListener('mousemove', function (e){
-			console.log('x: ' + e.x + ' y: ' + e.y);			   
+			//console.log('x: ' + e.x + ' y: ' + e.y);			   
 			mousePosX = e.x;
 			mousePosY = e.y;
-		
+		//console.log('x: ' + mousePosX + ' y: ' + mousePosY);
+        GetAngleOfMouse(mousePosX, mousePosY);
 	});
 	
 	
+}
+
+function GetAngleOfMouse(x,y)
+{
+    //this is not transfering over
+    //position of bow
+    thisAngle = CalculateAngle(10, 670, x, y);
+    finalAngle = Math.round(thisAngle);
+    console.log('Angle: ' + finalAngle);			   
+}
+
+function CalculateAngle(cx, cy, ex, ey) {
+  var dy = ey - cy;
+  var dx = ex - cx;
+  var theta = Math.atan2(dy, dx); // range (-PI, PI]
+  theta *= 180 / Math.PI; // rads to degs, range (-180, 180]
+  //if (theta < 0) theta = 360 + theta; // range [0, 360)
+  return theta;
 }
