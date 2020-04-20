8aa5bb35f32f91401422b54698c3504b87ce915b
Kelly <kiherstine@gmail.com>
2020-04-20 14:43:06 -0400

    Got bow properly rotating now!

--- WebsiteFiles/js/Bow.js
+++ WebsiteFiles/js/Bow.js
@@ -7,25 +7,31 @@ var bowY = 550;
 var cx = bowX + 0.5 * 120;
 var cy = bowY + 0.5 * 120;
 
-var angle = -25 ;
+var angle = -25;
 
+function RotateBow() {
+
+
+	//draw
+	//need to draw on every second
+	var temp = setInterval(function ShootArrow() {
+		DrawBow();
+		DrawPlayer();
+		ctx.clearRect(bowX, bowY, 120, 120);
+	}, 10);
+
+}
 function DrawBow()
 {
-    var bow = new Image();
-    bow.src = "Images/bowAndHands.png";
-	
-    //draw
-	//need to draw on every second
-    bow.onload = function () 
-    {
-        ctx2.imageSmoothingEnabled = true;
-		ctx2.save();
-		ctx2.translate(cx,cy);
-        ctx2.rotate( (Math.PI / 180) * angle);
-        ctx2.translate(-cx,-cy);
-   		ctx2.drawImage(bow, 0, 0, 120, 120, bowX, bowY, bow.width, bow.height);
-		//ctx.clearRect(bowX, bowY, 120, 120);
-		ctx2.restore();
-    }
-
+	var bow = new Image();
+	bow.src = "Images/bowAndHands.png";
+	bow.onload = function () {
+			ctx2.imageSmoothingEnabled = true;
+			ctx2.save();
+			ctx2.translate(cx, cy);
+			ctx2.rotate((Math.PI / 180) * angle);
+			ctx2.translate(-cx, -cy);
+			ctx2.drawImage(bow, 0, 0, 120, 120, bowX, bowY, bow.width, bow.height);
+			ctx2.restore();
+		}
 }
